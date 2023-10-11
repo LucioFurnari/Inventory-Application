80dc9de6c27c621d5b81978a6713d02c1e2a9884
LucioFurnari <luciofurnari99@gmail.com>
2023-10-10 23:27:45 -0300

    Create Item model schema for mongodb

--- models/item.js
+++ models/item.js
@@ -3,5 +3,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-  
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true, maxLength: 100 },
+  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
 })
+
+// Virtual for Item url
+ItemSchema.virtual('url').get(function () {
+  return `/items/${this._id}`
+})
+
+// Export model
+module.exports = mongoose.model('Item', ItemSchema);
