029ada7cf5553da6849a4ba096e995b807b4537d
LucioFurnari <luciofurnari99@gmail.com>
2023-10-12 23:04:50 -0300

    Create controllers folder and category, item controller

---
+++ controllers/itemController.js
@@ -0,0 +1,43 @@
+const Item = require('../models/item');
+const asyncHandler = require('express-async-handler');
+
+// Display list of all Items.
+exports.item_list = asyncHandler(async (req, res, next) => {
+  
+});
+
+// Display detail page for a specific Item
+exports.item_detail = asyncHandler(async (req, res, next) => {
+
+});
+
+// Display Item create form on GET.
+exports.item_create_get = asyncHandler(async (req, res, next) => {
+
+});
+
+// Handle Item create on POST.
+exports.item_create_post = asyncHandler(async (req, res, next) => {
+
+});
+
+// Display Item delete form on GET.
+exports.item_delete_get = asyncHandler(async (req, res, next) => {
+
+});
+
+// Handle Item delete on POST.
+exports.item_delete_post = asyncHandler(async (req, res, next) => {
+
+});
+
+// Display Item update form on GET.
+exports.item_update_get = asyncHandler(async (req, res, next) => {
+
+});
+
+// Handle Item update on POST.
+exports.item_update_post = asyncHandler (async (req, res, next) => {
+
+});
+
