8cbb5150ea1bb28440298024b7bdcf89dc6b5dd6
LucioFurnari <luciofurnari99@gmail.com>
2023-10-18 19:00:26 -0300

    Create routes to show catagories and items

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,12 +3,17 @@ const router = express.Router();
 
 // Require controller modules.
 const item_controller = require('../controllers/itemController');
+const category_controller = require('../controllers/categoryController');
 
+/// CATEGORY ROUTES ///
+router.get('/', category_controller.categories_list);
+
+// GET item list for category.
+router.get('/category/:id', item_controller.item_list);
 
 /// ITEM ROUTES ///
 
-// GET item list for category.
-router.get('/category/:id/items', item_controller.item_list);
-
 // GET detail for one Item.
-router.get('/category/:id/item/:id', item_controller.item_detail);
+router.get('/item/:id', item_controller.item_detail);
+
+module.exports = router;
