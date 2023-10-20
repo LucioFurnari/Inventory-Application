409f5c73dde18d0cc653f6261d003fd0f1ea607a
LucioFurnari <luciofurnari99@gmail.com>
2023-10-19 23:01:19 -0300

    Add products and item create routes

--- routes/inventory.js
+++ routes/inventory.js
@@ -6,12 +6,21 @@ const item_controller = require('../controllers/itemController');
 const category_controller = require('../controllers/categoryController');
 
 /// CATEGORY ROUTES ///
-router.get('/', category_controller.categories_list);
+router.get('/', category_controller.index);
+
+// Get category list
+router.get('/categories', category_controller.categories_list);
 
 // GET item list for category.
 router.get('/category/:id', item_controller.item_list);
 
 /// ITEM ROUTES ///
+
+// Get all Products.
+router.get('/products', item_controller.products_list)
+
+// GET item form.
+router.get('/item/create', item_controller.item_create_get);
 
 // GET detail for one Item.
 router.get('/item/:id', item_controller.item_detail);
