a80b3a05daad5d376c71f4d6bdb322d2e0e01291
LucioFurnari <luciofurnari99@gmail.com>
2023-10-24 17:38:16 -0300

    Add item, category create, and item update get and post controllers

--- routes/inventory.js
+++ routes/inventory.js
@@ -11,6 +11,9 @@ router.get('/', category_controller.index);
 // Get category list
 router.get('/categories', category_controller.categories_list);
 
+// GET category form.
+router.get('/category/create', category_controller.category_create_get);
+
 // GET item list for category.
 router.get('/category/:id', item_controller.item_list);
 
@@ -22,7 +25,16 @@ router.get('/products', item_controller.products_list)
 // GET item form.
 router.get('/item/create', item_controller.item_create_get);
 
+// POST item create
+router.post('/item/create', item_controller.item_create_post);
+
 // GET detail for one Item.
 router.get('/item/:id', item_controller.item_detail);
+
+// GET update form for Item.
+router.get('/item/:id/update', item_controller.item_update_get);
+
+// POST update form for Item.
+router.post('/item/:id/update', item_controller.item_update_post);
 
 module.exports = router;
