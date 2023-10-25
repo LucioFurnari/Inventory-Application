eecb68d2825337c4d1d1c5acf28adc38d753019e
LucioFurnari <luciofurnari99@gmail.com>
2023-10-25 19:49:56 -0300

    Move item list controller to category, and change view file

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,7 @@
 const Category = require('../models/category');
 const Item = require('../models/item');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 // Display Inventory info
 exports.index = asyncHandler(async (req, res, next) => {
@@ -19,15 +20,41 @@ exports.categories_list = asyncHandler(async (req, res, next) => {
   res.render('categories_list', { title: 'Categories list', categories_list: allCategories })
 });
 
+// Display list of all Items for category.
+exports.item_list = asyncHandler(async (req, res, next) => {
+  const allItems = await Item.find({ category: req.params.id}).exec();
+
+  res.render('category_detail', { title: 'Item list', items_list: allItems })
+});
+
 // Display Category create form on GET
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-
+  res.render('category_form', {})
 });
 
 // Handle Category create on POST
-exports.category_create_post = asyncHandler(async (req, res, next) => {
+exports.category_create_post = [
+  body('category_name', 'Empty name').trim().isLength({ min: 1}).escape(),
+  body('category_description', 'The description should not be empty').trim().isLength({ min: 1}).escape(),
 
-});
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.category_name,
+      description: req.body.category_description,
+    })
+
+    if (!errors.isEmpty()) {
+      res.render('category_form', {errors: errors.array()})
+      return;
+    } else {
+      // Data from form is valid.
+      await category.save();
+      res.redirect(category.url)
+    }
+  }),
+]
 
 // Display Category update form on GET
 exports.category_update_get = asyncHandler(async (req, res, next) => {
