b9a0015e04e946ae5885661feecb1ecb9fbd8613
LucioFurnari <luciofurnari99@gmail.com>
2023-11-14 00:35:25 -0300

    Remove console log and unused code

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,9 @@
 function activeNavLinks () {
   const links = document.querySelectorAll('.list-nav a');
-  console.log(links)
   const actualPath = window.location.pathname;
+
   links.forEach((item) => {
-    if (actualPath === '/inventory' && item.getAttribute('href') === '/') {
-      item.classList.add('active');
-    } else if (item.getAttribute('href') === actualPath) {
+    if (item.getAttribute('href') === actualPath) {
       item.classList.add('active');
     } else {
       item.classList.remove('active');
